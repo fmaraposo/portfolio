cd56d783e76f010010917bf46435c4c7ece2ebef
Francisco Martins <fmaraposo@gmail.com>
2021-04-04 17:19:31 +0200

    Adding Text to Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,14 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <i class="fas fa-camera fa-3x"></i>
-        </div>
+        <React.Fragment>
+            <div style={{display:'flex'}}>
+                <i className='fas fa-bars fa-3x' style={{cursor:'pointer', margin:'0.5em 0 0 1em'}}></i>
+            </div>
+            <div className="HomeContainerText">
+                <h1 className="HomeText">Hey. I'm Francisco! <br/> I am a Web Developer.</h1>
+            </div>
+        </React.Fragment>
     )
 }
 
